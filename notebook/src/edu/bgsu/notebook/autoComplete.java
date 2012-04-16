7e764eb23fe4c44258eaf8ebb0267df005cc4441
Baers <matt2k10@gmail.com>
2012-04-16 06:08:03 -0400

    Fixed speed issues with autoComplete.search() by switching to HashMap for the dictionary. Added test file and two dictionaries. Please test them out!

--- notebook/src/edu/bgsu/notebook/autoComplete.java
+++ notebook/src/edu/bgsu/notebook/autoComplete.java
@@ -1,7 +1,7 @@
-/*
+/**
  * autoComplete.java
  * 
- * Author: Matthew Baer
+ * @author Matthew Baer
  * 
  * Purpose: A wrapper class for the autoComplete search function. The UI will call this function with the current text
  * 			that the user has entered as an argument. The job of this function is to return the five shortest words from a dictionary
@@ -12,11 +12,21 @@
 
 package edu.bgsu.notebook;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.io.*;
 
 public class autoComplete {
 
+	//HashMap that stores LinkedLists for every letter of the alphabet. After loading, all the words beginning with that letter will belong to the corresponding LinkedList
+	HashMap<String, LinkedList<String>> dictionary = new HashMap<String, LinkedList<String>>();
+	
+	//Dictionary file location + name
+	String dict_loc = "dictionary/74550com.mon";
+	//String dict_loc = "dictionary/354984si.ngl";
+	
 	/**
 	 * autoComplete.search()
 	 * The UI will call this function with the current text that the user has entered as an argument. 
@@ -28,49 +38,44 @@ public class autoComplete {
 	 * @throws IOException
 	 */
 	public LinkedList<String> search(String substring) throws IOException {
-		String temp;
+		String temp, substring_l, key;
 		int i = 0, x;
 		boolean foundall = false;
 		boolean firstUpper = false, allUpper = false;
-		LinkedList<String> dictionary = new LinkedList<String>(); //Linked list of Strings to store the words from the dictionary file
-		LinkedList<String> results = new LinkedList<String>(); //Stores the top n matches
+		int maxMatches = 5;
+		LinkedList<String> results = new LinkedList<String>(); //Stores the top maxMatches # of matches
+		
+		//Substring is empty, has numbers, or only 1 character, so return immediately with a blank result list
+		if(substring.isEmpty() || substring.length() == 1 || hasDigits(substring))
+			return results;
+		
 		
 		/*
 		 * Check the case of the substring. There are two possible cases we will replicate in the results:
 		 * 1. All uppercase
 		 * 2. First letter capitalized
 		 * 
-		 * RaNdOm cApitAlIZATION will be ignored. It's much more helpful to provide a result with correct capitalization, i.e. a word that should be capitalized.
+		 * RaNdOm cApitAlIZATIoN will be ignored. It's much more helpful to provide a result with correct capitalization, i.e. a word that should be capitalized.
 		 */
 		if(substring.compareTo(substring.toUpperCase()) == 0)
 			allUpper = true;
 		else if(Character.isUpperCase(substring.charAt(0)))
 			firstUpper = true;
 		
-		//Create the input stream to prep the dictionary file for reading
-		InputStream in = this.getClass().getResourceAsStream("edu.bgsu.notebook.dictionary/74550com.mon");
+		//Make the substring lower case for comparing
+		substring_l = substring.toLowerCase();
 		
-		//Create a buffered reader to read in the dictionary file
-		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		//Pull off the first character to get HashMap key
+		key = substring.substring(0, 1);
 		
-		//Read the first line from the file
-		temp = br.readLine();
-		
-		//Loop until we reach the end of the file
-		while(temp != null) {
-			//Add the line to the dictionary linked list
-			dictionary.add(temp);
-			
-			//Read the next line
-			temp = br.readLine();
-		}
+		//Load the dictionary into memory
+		loadDictionary();
 		
 		//Search the dictionary file
-		//for(int i = 0; i < dictionary.size(); i++) 
-		while(i < dictionary.size() && !foundall){
-			temp = dictionary.get(i);
+		while(i < dictionary.get(key).size() && !foundall){
+			temp = dictionary.get(key).get(i);
 			//If we find a word that contains the substring at the beginning, add it to the results list
-			if(temp.matches(substring.toLowerCase() + ".*") || temp.equalsIgnoreCase(substring)) {
+			if(temp.matches(substring_l + ".*")) {
 				
 				//Adjust the case of the matching word before adding to the list of found words
 				if(allUpper)
@@ -82,7 +87,7 @@ public class autoComplete {
 				results.add(temp);
 				
 				//If this is not the last word, search for more matches
-				if(i != dictionary.size() - 1){
+				if(i != dictionary.get(key).size() - 1){
 					/*
 					 * Search for other matches. Since the list is alphabetized, these should appear ONLY directly below the first match and nowhere else.
 					 */
@@ -91,10 +96,10 @@ public class autoComplete {
 					x = i + 1;
 					
 					//Get the next word
-					temp = dictionary.get(x);
+					temp = dictionary.get(key).get(x);
 					
-					//Loop until we get five results, hit a result that doesn't match, or reach the end of the dictionary
-					while(x < dictionary.size() && x < i + 5 && (temp.matches(substring.toLowerCase() + ".*") || temp.equalsIgnoreCase(substring))) {
+					//Loop until we get maxMatches # of results, hit a result that doesn't match, or reach the end of the dictionary
+					while(x < dictionary.get(key).size() && x < i + maxMatches && (temp.matches(substring_l + ".*"))) {
 						//Adjust the case of the matching word before adding to the list of found words
 						if(allUpper)
 							temp = temp.toUpperCase();
@@ -110,7 +115,7 @@ public class autoComplete {
 						x++;
 						
 						//Get the next word
-						temp = dictionary.get(x);
+						temp = dictionary.get(key).get(x);
 					}
 					
 					//Found all matches
@@ -125,8 +130,49 @@ public class autoComplete {
 			i++;
 		}
 		
-		
 		//Return matches. This will be empty if nothing is found.
 		return results;
+	}
+	
+	
+	/**
+	 * autoComplete.loadDictionary
+	 * Loads the dictionary file into the HashMap for searching.
+	 * @throws IOException
+	 */
+	private void loadDictionary() throws IOException {
+		String temp;
+		
+		//Create the input stream to prep the dictionary file for reading
+		InputStream in = this.getClass().getResourceAsStream(dict_loc);
+		
+		//Create a buffered reader to read in the dictionary file
+		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		
+		//Read the first line from the file
+		temp = br.readLine();
+		
+		//Loop until we reach the end of the file
+		while(temp != null) {
+			
+			//Create a key for this letter if it does not already exist
+			if(!dictionary.containsKey(temp.substring(0, 1)))
+				dictionary.put(temp.substring(0, 1), new LinkedList<String>());
+			
+			//Add the line to the dictionary linked list
+			dictionary.get(temp.substring(0, 1)).add(temp);
+			
+			//Read the next line
+			temp = br.readLine();
+		}
+	}
+	
+	private boolean hasDigits(String substring) {
+		for(int i = 0; i < substring.length(); i++) {
+			if((Character.isDigit(substring.charAt(i))))
+					return true;
+		}
+		
+		return false;
 	}
 }
